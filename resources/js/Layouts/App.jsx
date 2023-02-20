2899224daec938b0482a594d7efe6d1586b6555c
Sitt Aung <sittaung07@gmail.com>
2023-02-20 15:15:14 +0630

    Routing - active links and apply different css classes to them

--- resources/js/Layouts/App.jsx
+++ resources/js/Layouts/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import PostsIndex from "../Pages/Posts";
 import PostsCreate from "../Pages/Posts/Create";
 
@@ -15,18 +15,26 @@ function App() {
                                 </div>
 
                                 <div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                                    <Link
+                                    <NavLink
                                         to="/"
-                                        className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
+                                        className={({ isActive }) =>
+                                            isActive
+                                                ? "inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out"
+                                                : "inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
+                                        }
                                     >
                                         Posts
-                                    </Link>
-                                    <Link
+                                    </NavLink>
+                                    <NavLink
                                         to="/posts/create"
-                                        className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
+                                        className={({ isActive }) =>
+                                            isActive
+                                                ? "inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out"
+                                                : "inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
+                                        }
                                     >
                                         Add post
-                                    </Link>
+                                    </NavLink>
                                 </div>
                             </div>
                         </div>
