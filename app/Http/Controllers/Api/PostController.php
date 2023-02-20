b448827ac2b5b185a342e6a30af35dbe96d1c1a6
Sitt Aung <sittaung07@gmail.com>
2023-02-20 16:59:51 +0630

    Store post and navigate back

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -41,7 +41,13 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $post = Post::create([
+            'title' => $request->title,
+            'content' => $request->content,
+            'category_id' => $request->category_id,
+        ]);
+
+        return new PostResource($post);
     }
 
     /**
