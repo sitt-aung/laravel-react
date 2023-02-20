f4757726de440342f187430d90764e83fb9612ab
Sitt Aung <sittaung07@gmail.com>
2023-02-20 15:08:25 +0630

    One routing fix for SPAs

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'layouts.app');
 
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
-
-require __DIR__.'/auth.php';
+Route::view('/{any?}', 'layouts.app')->where('any', '.*');
