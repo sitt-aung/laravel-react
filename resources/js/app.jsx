87eebbd5f3d16e2c4c6b1f62250b80a21a55e636
Sitt Aung <sittaung07@gmail.com>
2023-02-20 15:04:27 +0630

    Routing - layout, navigation links and post create page

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,7 +1,7 @@
 import './bootstrap'
 
 import { createRoot } from 'react-dom/client';
-import PostsIndex from './Pages/Posts/Index';
+import App from './Layouts/App';
 
 const root = createRoot(document.getElementById('app'));
-root.render(<PostsIndex />);
+root.render(<App />);
--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'dashboard')->name('dashboard');
+Route::view('/', 'layouts.app');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
 require __DIR__.'/auth.php';
