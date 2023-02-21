69873b6babfb65b0742d6732699e5e55c0aa757c
Sitt Aung <sittaung07@gmail.com>
2023-02-21 14:54:50 +0630

    Delete post with sweet alert confirmation

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -73,8 +73,10 @@ class PostController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Post $post)
     {
-        //
+        $post->delete();
+
+        return response()->noContent();
     }
 }
--- resources/js/Pages/Posts/Index.jsx
+++ resources/js/Pages/Posts/Index.jsx
@@ -2,108 +2,179 @@ import axios from "axios";
 import { Component } from "react";
 import CategoriesService from "../../Services/CategoriesService";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 class PostsIndex extends Component {
     constructor(props) {
         super(props);
-        
+
         this.state = {
             posts: [],
             categories: [],
             query: {
                 page: 1,
-                category_id: '',
-                order_column: 'id',
-                order_direction: 'desc',
-            }
-        }
+                category_id: "",
+                order_column: "id",
+                order_direction: "desc",
+            },
+        };
 
         this.categoryChanged = this.categoryChanged.bind(this);
         this.pageChanged = this.pageChanged.bind(this);
-        this.orderChanged = this.orderChanged.bind(this)
+        this.orderChanged = this.orderChanged.bind(this);
+        this.deletePost = this.deletePost.bind(this);
     }
 
     fetchPosts() {
-        axios.get('/api/posts', { params:  this.state.query })
-            .then(response => this.setState({ posts: response.data }))
+        axios
+            .get("/api/posts", { params: this.state.query })
+            .then((response) => this.setState({ posts: response.data }));
     }
 
     pageChanged(url) {
         const fullUrl = new URL(url);
 
-        this.setState(({
-            query: {
-                page: fullUrl.searchParams.get('page')
-            }
-        }), () => this.fetchPosts())
+        this.setState(
+            {
+                query: {
+                    page: fullUrl.searchParams.get("page"),
+                },
+            },
+            () => this.fetchPosts()
+        );
     }
 
     categoryChanged(event) {
-        this.setState(({
-            query: {
-                category_id: event.target.value,
-                page: 1
-            }
-        }), () => this.fetchPosts())
+        this.setState(
+            {
+                query: {
+                    category_id: event.target.value,
+                    page: 1,
+                },
+            },
+            () => this.fetchPosts()
+        );
     }
 
     componentDidMount() {
-        CategoriesService.getAll()
-            .then(response => this.setState({ categories: response.data }))
-        this.fetchPosts()
+        CategoriesService.getAll().then((response) =>
+            this.setState({ categories: response.data })
+        );
+        this.fetchPosts();
+    }
+
+    deletePost(event) {
+        Swal.fire({
+            title: "Delete this post?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonText: "Yes",
+            confirmButtonColor: "#EF4444",
+            cancelButtonText: "No",
+            cancelButtonColor: "#A3A3A3",
+            reverseButtons: true,
+            focusCancel: true,
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axios
+                    .delete("/api/posts/" + event.target.value)
+                    .then((response) => this.fetchPosts())
+                    .catch((error) =>
+                        Swal.fire({
+                            icon: "error",
+                            title: "Something went wrong",
+                        })
+                    );
+            }
+        });
     }
 
     renderPosts() {
-        return this.state.posts.data.map(post => <tr key={post.id}>
-            <td>{ post.id }</td>
-            <td>{ post.title }</td>
-            <td>{ post.category.name }</td>
-            <td>{ post.content }</td>
-            <td>{ post.created_at }</td>
-            <td><Link to={`posts/edit/${post.id}`}>Edit</Link></td>
-        </tr>);
+        return this.state.posts.data.map((post) => (
+            <tr key={post.id}>
+                <td>{post.id}</td>
+                <td>{post.title}</td>
+                <td>{post.category.name}</td>
+                <td>{post.content}</td>
+                <td>{post.created_at}</td>
+                <td>
+                    <Link to={`posts/edit/${post.id}`}>Edit</Link>
+                    <button
+                        value={post.id}
+                        onClick={this.deletePost}
+                        type="button"
+                        className="bg-red-500 rounded-full text-white px-3 py-1 font-bold"
+                    >
+                        Delete
+                    </button>
+                </td>
+            </tr>
+        ));
     }
 
     renderCategoryFilter() {
-        const categories = this.state.categories.data.map(category =>
-            <option key={category.id} value={category.id}>{ category.name }</option>
-        );
+        const categories = this.state.categories.data.map((category) => (
+            <option key={category.id} value={category.id}>
+                {category.name}
+            </option>
+        ));
 
         return (
-            <select onChange={this.categoryChanged} className="mt-1 sm:mt-0 sm:w-1/4 rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+            <select
+                onChange={this.categoryChanged}
+                className="mt-1 sm:mt-0 sm:w-1/4 rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            >
                 <option>-- all categories --</option>
-                { categories }
+                {categories}
             </select>
-        )
+        );
     }
 
     renderPaginatorLinks() {
-        return this.state.posts.meta.links.map((link, index) => 
-            <button key={index} onClick={() => this.pageChanged(link.url)} dangerouslySetInnerHTML={{__html: link.label}} className="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 hover:text-gray-500 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150 first:rounded-l-md last:rounded-r-md" />
-        );
+        return this.state.posts.meta.links.map((link, index) => (
+            <button
+                key={index}
+                onClick={() => this.pageChanged(link.url)}
+                dangerouslySetInnerHTML={{ __html: link.label }}
+                className="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 hover:text-gray-500 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150 first:rounded-l-md last:rounded-r-md"
+            />
+        ));
     }
 
     renderPaginator() {
         return (
-            <nav role="navigation" aria-label="Pagination Navigation" className="flex items-center justify-between">
+            <nav
+                role="navigation"
+                aria-label="Pagination Navigation"
+                className="flex items-center justify-between"
+            >
                 <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
                     <div>
                         <p className="text-sm text-gray-700 leading-5">
                             Showing
                             <span>
-                                <span className="font-medium"> { this.state.posts.meta.from } </span>
+                                <span className="font-medium">
+                                    {" "}
+                                    {this.state.posts.meta.from}{" "}
+                                </span>
                                 to
-                                <span className="font-medium"> { this.state.posts.meta.to } </span>
+                                <span className="font-medium">
+                                    {" "}
+                                    {this.state.posts.meta.to}{" "}
+                                </span>
                             </span>
                             of
-                            <span className="font-medium"> { this.state.posts.meta.total } </span>
+                            <span className="font-medium">
+                                {" "}
+                                {this.state.posts.meta.total}{" "}
+                            </span>
                             results
                         </p>
                     </div>
 
                     <div>
                         <span className="relative z-0 inline-flex shadow-sm rounded-md">
-                            { this.renderPaginatorLinks() }
+                            {this.renderPaginatorLinks()}
                         </span>
                     </div>
                 </div>
@@ -112,60 +183,72 @@ class PostsIndex extends Component {
     }
 
     orderColumnIcon(column) {
-        let icon = 'fa-sort';
+        let icon = "fa-sort";
         if (this.state.query.order_column === column) {
-            if (this.state.query.order_direction === 'asc') {
-                icon = 'fa-sort-up';
+            if (this.state.query.order_direction === "asc") {
+                icon = "fa-sort-up";
             } else {
-                icon = 'fa-sort-down';
+                icon = "fa-sort-down";
             }
         }
 
-        return (
-            <i className={`fa-solid ${icon}`}></i>
-        )
+        return <i className={`fa-solid ${icon}`}></i>;
     }
 
     orderChanged(column) {
-        let direction = 'asc';
+        let direction = "asc";
         if (column === this.state.query.order_column) {
-            direction = this.state.query.order_direction === 'asc' ? 'desc' : 'asc';
+            direction =
+                this.state.query.order_direction === "asc" ? "desc" : "asc";
         }
 
-        this.setState(({
-            query: {
-                page: 1,
-                order_column: column,
-                order_direction: direction,
-            }
-        }), () => this.fetchPosts())
+        this.setState(
+            {
+                query: {
+                    page: 1,
+                    order_column: column,
+                    order_direction: direction,
+                },
+            },
+            () => this.fetchPosts()
+        );
     }
 
     render() {
-        if (!('data' in this.state.posts)) return;
+        if (!("data" in this.state.posts)) return;
 
         return (
             <div className="overflow-hidden overflow-x-auto p-6 bg-white border-gray-200">
                 <div className="min-w-full align-middle">
-                    <div className="mb-4">
-                        { this.renderCategoryFilter() }
-                    </div>
+                    <div className="mb-4">{this.renderCategoryFilter()}</div>
                     <table className="table">
                         <thead className="table-header">
                             <tr>
                                 <th>
                                     <div>
                                         <span>ID</span>
-                                        <button onClick={() => this.orderChanged('id')} type="button" className="column-sort">
-                                            { this.orderColumnIcon('id') }
+                                        <button
+                                            onClick={() =>
+                                                this.orderChanged("id")
+                                            }
+                                            type="button"
+                                            className="column-sort"
+                                        >
+                                            {this.orderColumnIcon("id")}
                                         </button>
                                     </div>
                                 </th>
                                 <th>
                                     <div>
                                         <span>Title</span>
-                                        <button onClick={() => this.orderChanged('title')} type="button" className="column-sort">
-                                            { this.orderColumnIcon('title') }
+                                        <button
+                                            onClick={() =>
+                                                this.orderChanged("title")
+                                            }
+                                            type="button"
+                                            className="column-sort"
+                                        >
+                                            {this.orderColumnIcon("title")}
                                         </button>
                                     </div>
                                 </th>
@@ -188,16 +271,14 @@ class PostsIndex extends Component {
                             </tr>
                         </thead>
                         <tbody className="table-body">
-                            { this.renderPosts() }
+                            {this.renderPosts()}
                         </tbody>
                     </table>
-                    <div className="mt-4">
-                        { this.renderPaginator() }
-                    </div>
+                    <div className="mt-4">{this.renderPaginator()}</div>
                 </div>
             </div>
         );
     }
 }
 
-export default PostsIndex
+export default PostsIndex;
